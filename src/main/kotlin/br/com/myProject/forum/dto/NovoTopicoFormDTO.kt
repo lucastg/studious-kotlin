5feea4fbbd90a65add1b50e798fba3c1d597d112
lucas <lucasU37@gmail.com>
2023-04-04 21:29:14 -0300

    Ajuste das anotaçoes no dto.

--- src/main/kotlin/br/com/myProject/forum/dto/NovoTopicoFormDTO.kt
+++ src/main/kotlin/br/com/myProject/forum/dto/NovoTopicoFormDTO.kt
@@ -1,16 +1,17 @@
 package br.com.myProject.forum.dto
 
 import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 data class NovoTopicoFormDTO(
     @field:NotEmpty
-    @Size(min = 5, max = 100)
+    @field:Size(min = 5, max = 100)
     var titulo: String,
     @field:NotEmpty
     val mensagem: String,
-    @field:NotEmpty
+    @field:NotNull
     val idCurso: Long,
-    @field:NotEmpty
+    @field:NotNull
     val idAutor: Long
 )
